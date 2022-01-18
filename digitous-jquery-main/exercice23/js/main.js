a13a55afd24dc9b4ce0fd80222e1778ed80256d0
unknown <clamy6@yahoo.fr>
2022-01-18 14:16:04 +0100

    ex23

---
+++ digitous-jquery-main/exercice23/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $(".btn").click(function(){
+        $("#square").animate({
+            marginLeft: "980px",
+            marginTop: "135px"
+
+
+        }, function() {
+
+        })
+    })
+})

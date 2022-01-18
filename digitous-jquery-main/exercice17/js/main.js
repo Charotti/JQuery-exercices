5e75ab2ee3add050f9220c93a2c82b312537cd02
unknown <clamy6@yahoo.fr>
2022-01-18 03:41:52 +0100

    ex17&18

---
+++ digitous-jquery-main/exercice17/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".btn").click(function(){
+         displaySecretText().show()
+           
+    })
+})
+
+//Propriété CSS display: block
---
+++ digitous-jquery-main/exercice18/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $(".btn").click(function(){
+        $("#text").hide()
+    })
+})
+
+// correspond à la règle CSS display: None
+

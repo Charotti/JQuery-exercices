d5b08c219876b2ae8a06885e48c34d930defaa71
unknown <clamy6@yahoo.fr>
2022-01-18 19:19:32 +0100

    ex30

---
+++ digitous-jquery-main/exercice30/js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+ 
+    const text = $("input");
+    text.keyup(function () {
+      if ((text.val().length <= 5)) {
+          // console.log(text);
+        $(text).addClass("is-invalid") 
+      } else {
+        $(text).removeClass("is-invalid").addClass("is-valid") 
+        // console.log(is-invalid);
+      
+      }
+    });
+ 
+});
